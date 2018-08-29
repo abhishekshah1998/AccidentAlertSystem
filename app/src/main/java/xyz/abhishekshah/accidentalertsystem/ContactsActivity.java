d00e3b31e822ef9de008a12f521d49d07fddb6b5
Abhishek <abhishekshah1998@gmail.com>
2018-08-29 11:21:11 +0530

    SMS Intent successful.

--- app/src/main/java/xyz/abhishekshah/accidentalertsystem/ContactsActivity.java
+++ app/src/main/java/xyz/abhishekshah/accidentalertsystem/ContactsActivity.java
@@ -13,6 +13,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+
+import java.util.List;
+import static android.Manifest.permission.SEND_SMS;
+
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -21,9 +37,9 @@ public class ContactsActivity extends AppCompatActivity {
     EditText editName,editNumber,editId;
 
     private static final int RESULT_PICK_CONTACT = 1;
+    private static final int REQUEST_SMS = 0;
 
-
-
+    private BroadcastReceiver sentStatusReceiver, deliveredStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,32 +65,174 @@ public class ContactsActivity extends AppCompatActivity {
         UpdateData();
         DeleteData();
         pickContact();
-        send();
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    int hasSMSPermission = checkSelfPermission(Manifest.permission.SEND_SMS);
+                    if (hasSMSPermission != PackageManager.PERMISSION_GRANTED) {
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.SEND_SMS)) {
+                            showMessageOKCancel("You need to allow access to Send SMS",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[] {Manifest.permission.SEND_SMS},
+                                                        REQUEST_SMS);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                        requestPermissions(new String[] {Manifest.permission.SEND_SMS},
+                                REQUEST_SMS);
+                        return;
+                    }
+                    sendMySMS();
+                }
+            }
+        });
 
     }
 
-    public void send(){
-        SendButton.setOnClickListener(
+    public void sendMySMS() {
 
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        sendSMS("9403955423", "Hi You got a message!");
+        String phone = "9028531389";
+        String message = "Helloo!";
+
+        //Check if the phoneNumber is empty
+        if (phone.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please Enter a Valid Phone Number", Toast.LENGTH_SHORT).show();
+        } else {
+
+            SmsManager sms = SmsManager.getDefault();
+            // if message length is too long messages are divided
+            List<String> messages = sms.divideMessage(message);
+            for (String msg : messages) {
+
+                PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+                PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+                sms.sendTextMessage(phone, null, msg, sentIntent, deliveredIntent);
+
+            }
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        sentStatusReceiver=new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Unknown Error";
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Sent Successfully !!";
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        s = "Generic Failure Error";
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        s = "Error : No Service Available";
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        s = "Error : Null PDU";
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        s = "Error : Radio is off";
+                        break;
+                    default:
+                        break;
+                }
+               // sendStatusTextView.setText(s);
+
+            }
+        };
+        deliveredStatusReceiver=new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Message Not Delivered";
+                switch(getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Delivered Successfully";
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
+               // deliveryStatusTextView.setText(s);
+               // phoneEditText.setText("");
+               // messageEditText.setText("");
+            }
+        };
+        registerReceiver(sentStatusReceiver, new IntentFilter("SMS_SENT"));
+        registerReceiver(deliveredStatusReceiver, new IntentFilter("SMS_DELIVERED"));
+    }
+
+
+
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(sentStatusReceiver);
+        unregisterReceiver(deliveredStatusReceiver);
+    }
+
+
+
+    private boolean checkPermission() {
+        return ( ContextCompat.checkSelfPermission(getApplicationContext(), SEND_SMS ) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{SEND_SMS}, REQUEST_SMS);
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_SMS:
+                if (grantResults.length > 0 &&  grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access sms", Toast.LENGTH_SHORT).show();
+                    sendMySMS();
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and sms", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(SEND_SMS)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{SEND_SMS},
+                                                        REQUEST_SMS);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
                     }
                 }
-        );
-
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
-    //Following code doesnot work
 
-    @SuppressWarnings("deprecation")
-    private void sendSMS(String phoneNumber, String message)
-    {
-        SmsManager smsManager =     SmsManager.getDefault();
-        smsManager.sendTextMessage("Phone Number", null, "Message", null, null);
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+                builder.setMessage(message);
+                builder.setPositiveButton("OK", okListener);
+                builder.setNegativeButton("Cancel", null);
+                android.support.v7.app.AlertDialog dialog = builder.create();
+                dialog.show();
+
+
     }
-
+    
     public void pickContact()
     {
        LoadButton.setOnClickListener(
